2b005b3cee7dbd94808895a5544af32d3f54b77c
Maureen Brennan <mbbrennan@gmail.com>
2020-09-26 12:32:54 -0600

    New Mo Controller (as a test)

---
+++ Controllers/MoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Hello Mo! Keep working at it!!";
+        }
+    }
+}
